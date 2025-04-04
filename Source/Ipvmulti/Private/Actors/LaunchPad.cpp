aa665adfa70b19793bdf2619b69c4f5f96b39838
RPin3 <113200552+RPin3@users.noreply.github.com>
2025-04-04 09:16:13 -0700

    Created BP_LaunchPad2-P1-AlanAlbinoTellezGironLizarraga

--- Source/Ipvmulti/Private/Actors/LaunchPad.cpp
+++ Source/Ipvmulti/Private/Actors/LaunchPad.cpp
@@ -14,6 +14,8 @@ ALaunchPad::ALaunchPad()
 	OverlapComp = CreateDefaultSubobject<UBoxComponent>("BoxComp");
 	RootComponent = OverlapComp;
 	MeshComp->SetupAttachment(OverlapComp);
+	LaunchForce = 1000;
+	LaunchAngle = 90;
 }
 
 // Called when the game starts or when spawned
@@ -30,8 +32,15 @@ void ALaunchPad::Tick(float DeltaTime)
 }
 
 void ALaunchPad::OverlapLaunchpad(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult & SweepResult){
+	FRotator LaunchDirection = GetActorRotation();
+	LaunchDirection.Pitch += LaunchAngle;
+	FVector LaunchVelocity = LaunchDirection.Vector()*LaunchForce;
+
+	
+	
 	ACharacter* MyCharacter = Cast<ACharacter>(OtherActor);
 	if (MyCharacter){
+		MyCharacter->LaunchCharacter(LaunchVelocity,true,true);
 		if (GEngine)
 		{
 			GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Green, TEXT("Overlap"));
--- Source/Ipvmulti/Public/Actors/LaunchPad.h
+++ Source/Ipvmulti/Public/Actors/LaunchPad.h
@@ -26,6 +26,13 @@ protected:
 	
 	UPROPERTY(VisibleAnywhere, Category = "Components")
 	UBoxComponent* OverlapComp;
+
+	UPROPERTY(EditAnywhere, Category = "Components")
+	float LaunchForce;
+
+	UPROPERTY(EditAnywhere, Category = "Components")
+	float LaunchAngle;
+
 	UFUNCTION()
 	void OverlapLaunchpad(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult & SweepResult);
 	
